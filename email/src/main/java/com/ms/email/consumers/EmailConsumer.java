f97209d8f1b1e11384dc0af0480252ae67122bcd
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2021-11-05 07:10:30 -0300

    Config RabbitMQ

---
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,31 @@
+package com.ms.email.consumers;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    private static final Logger logger = LogManager.getLogger(EmailConsumer.class);
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+
+        logger.debug("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+
+}
